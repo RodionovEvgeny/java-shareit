9038903b5659a55ffa045155283779d0d4c25a1b
EvgenyRodionov <globevgeny@yandex.ru>
2023-10-16 14:29:50 +0300

    feature: добавить бронирование в схему бд

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,34 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Tolerate;
+import ru.practicum.shareit.booking.dto.BookingStatus;
+import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Entity
+@Table(name = "bookings")
+@Data
+@Builder
 public class Booking {
+    @Id
+    private long id;
+    private Timestamp start;
+    private Timestamp end;
+    private Long item;
+    private Long booker;
+    private BookingStatus status;
+
+    @Tolerate
+    public Booking() {
+    }
 }
